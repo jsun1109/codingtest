5ab57073401f6f0fae1935bec1a2b7f881e6ff70
jsun1109 <161568081+jsun1109@users.noreply.github.com>
2024-04-26 09:12:02 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 62.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(angle: Int): Int {
+        var answer: Int = 0
+        answer = when (angle) {
+            in 1 until 90 -> 1
+            90 -> 2
+            in 91 until 180 -> 3
+            180 -> 4
+            else -> 5
+        }
+        return answer
+    }
+}
